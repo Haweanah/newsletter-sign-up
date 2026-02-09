ad9665a384f1cc298748e6790241a2bd2fad496c
Haweanah <ahauwa48@yahoo.com>
2026-02-09 13:10:15 +0100

    mobile view

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,14 @@
 export default function Form () {
     return (
-        <fieldset>
+        
   <form action="">
-    <label htmlFor="email">Email address</label>
-    <input 
+    <label className="email"
+    htmlFor="email">Email address</label>
+    <input
+    className="input"
     placeholder='email@company.com'
     type="email" name="email" id="email" />
-    <button type="submit">Subscribe to monthly newsletter</button>
+    <input type="submit" value="Subscribe to monthly newsletter" />
   </form>
-</fieldset>
-
     )
 }
