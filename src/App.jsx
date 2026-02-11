5a6d3a9c061ad1edab3525eec2fc2a18804a321c
Haweanah <ahauwa48@yahoo.com>
2026-02-11 12:01:07 +0100

    form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import NewsletterInfo from './components/NewsletterInfo'
 import Illustration from './components/Illustration'
@@ -5,14 +6,42 @@ import Footer from './components/Footer.jsx'
 import Form from "./components/Form"
 
 
+
+
 function App() {
+const [email, setEmail] = useState("");
+const [error, setError] = useState("");
+const [success, setSuccess] = useState(false);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    setError("Valid email required");
+    setSuccess(false)
+  } else {
+    setError("");
+    setSuccess(true)
+  }
+
+};
+
   return (
 
 
     <div className='container'>
     <Illustration/>
     <NewsletterInfo/>
-    <Form/>
+    <Form
+    handleSubmit={handleSubmit}
+    email={email}
+    setEmail={setEmail}
+    success={success}
+    error={error}
+    
+        />
 <Footer/>
 
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,24 @@
-export default function Form () {
+export default function Form (props) {
     return (
         
-  <form action="">
+  <form onSubmit={props.handleSubmit} noValidate>
     <label className="email"
     htmlFor="email">Email address</label>
+   
+   
     <input
     className="input"
     placeholder='email@company.com'
-    type="email" name="email" id="email" />
+    type="email" name="email" id="email"
+    value={props.email}
+    onChange={(e) => props.setEmail(e.target.value)
+    }
+    />
+
+    {props.error && <p className="error">{props.error}</p>}
+
     <input type="submit" value="Subscribe to monthly newsletter" />
+    {props.success && <p className="success">Subscription successful</p>}
   </form>
     )
 }
